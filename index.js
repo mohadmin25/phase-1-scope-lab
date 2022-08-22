09b05f9eafdfeb6142de7929c0c40e432186eb6c
mohadmin25 <abuhamar008@gmail.com>
2022-08-22 16:08:06 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = `bob`
+// customerName.toUpperCase()
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase() // declearing variable on global scope
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`
+}
+
+const leastFavoriteCustomer = `if bob`
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'what bob'
+}
